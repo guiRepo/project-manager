21ae0458c7794d6e8c8ffaed84926e07835fcf45
Guilherme André de Melo <guilhermeanm95@hotmail.com>
2025-05-07 21:37:03 -0300

    Criação de formulario de criação

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,8 @@
 export default defineAppConfig({
 	// https://ui.nuxt.com/getting-started/theme#design-system
 	ui: {
-		button: {
-			defaultVariants: {
-				// Set default button color to neutral
-				// color: 'neutral'
-			},
+		colors: {
+			primary: 'indigo'
 		}
-	},
-	
+	}
 });
