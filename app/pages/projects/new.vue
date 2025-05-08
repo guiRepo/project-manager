5b3e3763393c4ad6736b48b75cb83251c316e1ea
Guilherme André de Melo <guilhermeanm95@hotmail.com>
2025-05-08 01:49:23 -0300

    Conclusão formulario

--- app/pages/projects/new.vue
+++ app/pages/projects/new.vue
@@ -26,101 +26,93 @@
 
 <template>
 	<main class="container">
-		<div class="container-return">
-			<UIcon name="i-lucide-arrow-left" class="icon" />
-			<h2 class="text-return">Voltar</h2>
+		<div>
+			<NuxtLink to="/" class="container-return">
+				<UIcon name="i-lucide-arrow-left" class="icon" />
+				<h2 class="text-return">Voltar</h2>
+			</NuxtLink>
 		</div>
 		<h1 class="new-project-title">Novo Projeto</h1>
 
-	<section >
+	<section>
 		<div class="container-itens">
 			<form  @submit.prevent="handleSubmit">
-				<div>
+				<div class="container-input">
 					<label class="text-input">Nome do projeto <span class="required">(Obrigatório)</span></label>
-					<input v-model="form.nome" required class="w-196 p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400 text-black bg-white" type="text" />
+					<input v-model="form.nome" required class="w-196 p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400 text-black bg-white" type="text" >
 				</div>
 
-				<div>
+				<div class="container-input">
 					<label class="text-input">Cliente <span class="required">(Obrigatório)</span></label>
-					<input v-model="form.cliente" required class="w-196 p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400 text-black bg-white" type="text" />
+					<input v-model="form.cliente" required class="w-196 p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400 text-black bg-white" type="text" >
 				</div>
 
 				<div class="flex space-x-4">
 					<div class="flex-1">
 						<label class="text-input">Data de Início <span class="required">(Obrigatório)</span></label>
-						<input v-model="form.inicio" required class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400  text-black bg-white" type="date" />
+						<input v-model="form.inicio" required class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400  text-black bg-white accent-purple-500"  type="date">
 					</div>
 					<div class="flex-1">
 						<label class="text-input">Data Final <span class="required">(Obrigatório)</span></label>
-						<input v-model="form.fim" required class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400  text-black bg-white" type="date" />
+						<input v-model="form.fim" required class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-purple-400  text-black bg-white" type="date" >
 					</div>
     		</div>
-				<div>
+
+				<div class="cover-container">
 					<label class="text-input">Capa do projeto</label>
-					<div class="border-dashed border-2 border-gray-600 p-4 rounded text-center">
-						<input type="file" accept="image/png, image/jpeg" class="hidden" ref="fileInput" @change="handleFile" />
-						<div @click="$refs.fileInput.click()" class="cursor-pointer inline-block px-4 py-2 mt-2 text-gray-600 border border-gray-600 rounded hover:bg-purple-100">
-							Selecionar
-						</div>
+					<div class="file-container">
+						<UIcon name="i-lucide-hard-drive-upload" class="icon" />
+						<label class="required">Escolha uma imagem .jpg ou .png no seu dispositivo</label>
+						<input ref="fileInput"  type="file" accept="image/png, image/jpeg" class="hidden" @change="handleFile" >
+						<div class="file-button" @click="$refs.fileInput.click()">Selecionar</div>
 						<div v-if="form.capaPreview" class="mt-4">
-							<img :src="form.capaPreview" class="max-h-40 mx-auto rounded" />
+								<img :src="form.capaPreview" class="max-h-40 mx-auto rounded" >
 						</div>
 					</div>
 				</div>
 
-
-				<button type="submit" class="w-full py-3 bg-purple-400 text-white rounded-full hover:bg-purple-500 transition">
+				<button type="submit" class="salve-file">
 					Salvar projeto
 				</button>
 			</form>
-
-			<!-- <div class="container-input">
-				<label class="text-input">Nome do projeto <span class="required">(Obrigatório)</span></label>
-				<input class="input-name" type="text">
-			</div>
-			<div class="container-input">
-				<label class="text-input">Cliente <span class="required">(Obrigatório)</span></label>
-				<input class="input-name" type="text">
-			</div>
-
-			<div class="container-data">
-				<div class="container-input">
-					<label class="text-input">Nome do projeto <span class="required">(Obrigatório)</span></label>
-					<input class="input-data" type="text">
-				</div>
-				<div class="container-input">
-					<label class="text-input">Nome do projeto <span class="required">(Obrigatório)</span></label>
-					<input class="input-data" type="text">
-				</div>
-			</div>
-
-			<div class="container-input">
-				<label class="text-input">Cliente <span class="required">(Obrigatório)</span></label>
-				<button>
-					<input class="input-name" type="file">
-				</button>
-			</div> -->
 		</div>
 	</section>
 	</main>
 </template>
 
 <style scoped>
+	.file-container {
+		padding: 30px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-style: dashed;
+		border-width: 1px;
+		border-color: #717171;
+		border-radius: 4px;	
+		text-align: center;
+	}
 	.container-return {
 		display: flex;
 		flex-direction: row;
 	}
 	.text-return {
+		display: flex;
 		color: #695CCD;
 		font-family: Encode Sans Semi Expanded;
 		font-weight: 400;
-		font-size: 16px;
+		font-size: 19px;
 		line-height: 100%;
 		letter-spacing: 0%;
 		vertical-align: middle;
+		margin-top: 3px;
+		margin-left: 10px;
 	}
 	.icon {
 		color: #695CCD;
+		width: calc(var(--spacing) * 6) /* 1.5rem = 24px */;
+  	height: calc(var(--spacing) * 6) /* 1.5rem = 24px */;
 	}
 	.new-project-title {
 		color: #1F1283;
@@ -130,6 +122,7 @@
 		line-height: 100%;
 		letter-spacing: 0%;
 		vertical-align: middle;
+		margin-top: 15px;
 	}
 	.item-form {
 		display: flex;
@@ -161,6 +154,7 @@
 		border-width:  1px;
 		border-color: #DCDCDC;
 		border-radius: 8px;
+		border-style: dashed;
 		box-sizing: border-box;
 		
 		display: flex;
@@ -173,7 +167,6 @@
 		max-width: 100%; 
 
 	}
-
 	.container-data {
 		display: flex;
 		flex-direction: row;
@@ -187,10 +180,8 @@
 		border-radius: 4px;
 		border-width: 1px;
 	}
-
 	.container-input {
-		display: flex;
-		flex-direction: column;
+		margin-bottom: 30px;
 	}
 	.input-name {
 		width: 702px;
@@ -211,5 +202,35 @@
 		line-height: 22px;
 		letter-spacing: 0%;
 		margin-bottom: calc(var(--spacing) * 1) 
+	}
+	.file-button {
+		width: 144px;
+		cursor: pointer;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		padding-inline: calc(var(--spacing)*4);
+		padding-block: calc(var(--spacing)*2);
+		margin-top: calc(var(--spacing)*2);
+		
+		color: #695CCD;
+		border-style: var(--tw-border-style);
+		border-width: 1px;
+		border-color: #695CCD;
+		border-radius: 45px;
+	}
+	.cover-container {
+		display: flex;
+		flex-direction: column;
+		margin-top: 30px;
+	}
+	.salve-file {
+		width: 100%;
+		padding-block: calc(var(--spacing)*3);
+		background-color: #B2A8FF;
+		color: white;
+		border-radius: calc(infinity * 1px);
+		margin-top: 30px;
 	}
 </style>
